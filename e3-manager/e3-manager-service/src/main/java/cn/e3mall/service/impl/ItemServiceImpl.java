b8769708cece5bdc5601e88b56915451de7f2d71
wmy_near <wmy_near@163.com>
2017-08-15 20:08:43 +0800

    day02

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
 @Service
@@ -17,6 +24,25 @@ public class ItemServiceImpl implements ItemService {
 	public TbItem getItemById(long itemId) {
 		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
 		return tbItem;
+	}
+
+	@Override
+	public DataGridResult getItemList(int page, int rows) {
+		//1.设置分页信息
+		PageHelper.startPage(page, rows);
+		//2.执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//3.取查询结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		//取总记录数
+		long total = pageInfo.getTotal();
+		//4.把结果封装到DataGridResult中
+		DataGridResult result = new DataGridResult();
+		result.setRows(list);
+		result.setTotal(total);
+		//5.返回结果
+		return result;
 	}
 
 }
