69cf45745e1adf2491c830ddbcbcf32a62c07053
wmy_near <wmy_near@163.com>
2017-08-17 20:54:16 +0800

    day03

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,45 @@
+package cn.e3mall.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.common.pojo.TreeNode;
+import cn.e3mall.mapper.TbItemCatMapper;
+import cn.e3mall.pojo.TbItemCat;
+import cn.e3mall.pojo.TbItemCatExample;
+import cn.e3mall.pojo.TbItemCatExample.Criteria;
+import cn.e3mall.service.ItemCatService;
+
+@Service
+public class ItemCatServiceImpl implements ItemCatService {
+
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+	
+	@Override
+	public List<TreeNode> getItemCatList(long parentId) {
+		//1.根据parentId查询子节点列表
+		TbItemCatExample example = new TbItemCatExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andParentIdEqualTo(parentId);
+		//执行查询
+		List<TbItemCat> list = itemCatMapper.selectByExample(example);
+		//2.把列表转换成List<TreeNode>
+		List<TreeNode> treeNodes = new ArrayList<>();
+		for (TbItemCat itemCat : list) {
+			TreeNode treeNode = new TreeNode();
+			treeNode.setId(itemCat.getId());
+			treeNode.setText(itemCat.getName());
+			//父节点：close；叶子节点：open
+			treeNode.setState(itemCat.getIsParent()?"close":"open");
+			//添加到节点列表
+			treeNodes.add(treeNode);
+		}
+		//3.返回结果
+		return treeNodes;
+	}
+
+}
